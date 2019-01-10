3cd918b346191ea6ae7e556aff1bb2e9359318e3
kcyoow <kcy021012@gmail.com>
2019-01-10 14:05:01 +0900

    h

--- MY_NEWPROJECT/src/test/myproject/anything/Test.java
+++ MY_NEWPROJECT/src/test/myproject/anything/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 12
-		System.out.println("Hi Everyone!!");
+		System.out.println("Hi Everyone!!!");
 
 	}
 
